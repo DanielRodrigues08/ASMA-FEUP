14862c96f6afee68dbb3d9f98576716bcfc36b80
tomaspm98 <up202006845@up.pt>
2024-03-18 16:41:30 +0000

    Read from csv + convert values to float

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 
 def msg_orders_to_list(msg):
     orders = msg.body.split(" ")
@@ -7,11 +8,20 @@ def msg_orders_to_list(msg):
 
 def csv_centers_to_system(csv):
     df = pd.read_csv(csv, sep=';')
+    print(df)
+    df['latitude'] = df['latitude'].str.replace(',', '.')
+    df['longitude'] = df['longitude'].str.replace(',', '.')  
+    df['latitude'] = df['latitude'].astype(float)
+    df['longitude'] = df['longitude'].astype(float)  
     return df.values[0].tolist()
 
 def csv_orders_to_system(csv):
     df = pd.read_csv(csv, sep=';')
     orders = []
+    df['latitude'] = df['latitude'].str.replace(',', '.')
+    df['longitude'] = df['longitude'].str.replace(',', '.')  
+    df['latitude'] = df['latitude'].astype(float)
+    df['longitude'] = df['longitude'].astype(float)  
     for i in range(1,len(df)):
         current_row = df.iloc[i].values.tolist()
         orders.append(current_row)
@@ -19,6 +29,12 @@ def csv_orders_to_system(csv):
 
 def csv_drones_to_system(csv):
     df = pd.read_csv(csv, sep=';')
+    df['capacity'] = df['capacity'].str.replace('kg', '')
+    df['autonomy'] = df['autonomy'].str.replace('Km', '')
+    df['velocity'] = df['velocity'].str.replace('m/s', '')
+    df['capacity'] = df['capacity'].astype(float)
+    df['autonomy'] = df['autonomy'].astype(float)
+    df['velocity'] = df['velocity'].astype(float)
     drones = []
     for i in range(len(df)):
         current_row = df.iloc[i].values.tolist()
@@ -27,7 +43,7 @@ def csv_drones_to_system(csv):
         
 
 def main():
-    res = csv_orders_to_system("../delivery_center1.csv")    
+    res = csv_drones_to_system("../delivery_drones.csv")    
     print(res)
     
 if __name__ == "__main__":
