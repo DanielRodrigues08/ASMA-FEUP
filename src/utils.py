7be135da9c1c400f2e8473c9f1a3b1e3f7e5c401
tomaspm98 <up202006845@up.pt>
2024-03-18 16:14:22 +0000

    Adapted read_excel to new dataset

--- src/utils.py
+++ src/utils.py
@@ -5,26 +5,31 @@ def msg_orders_to_list(msg):
     orders = msg.body.split(" ")
     return orders
 
-def xsl_centers_to_system(xsl):
-    df = pd.read_excel(xsl, header = None)
-    centers = []
-    for i in range(len(df)):
-        if pd.isnull(df.iloc[i, 0]):
-            break
-        centers.append(df.iloc[i].values.tolist())
-    return centers  
+def csv_centers_to_system(csv):
+    df = pd.read_csv(csv, sep=';')
+    return df.values[0].tolist()
 
-def xsl_orders_to_system(xsl):
-    df = pd.read_excel(xsl, header = None)
-    header_row = [np.nan, 'weight', 'latitude', 'longitude']
+def csv_orders_to_system(csv):
+    df = pd.read_csv(csv, sep=';')
     orders = []
-    start_saving = False
+    for i in range(1,len(df)):
+        current_row = df.iloc[i].values.tolist()
+        orders.append(current_row)
+    return orders   
+
+def csv_drones_to_system(csv):
+    df = pd.read_csv(csv, sep=';')
+    drones = []
     for i in range(len(df)):
         current_row = df.iloc[i].values.tolist()
-        if current_row == header_row:
-            start_saving = True
-            continue 
-        if start_saving:
-            orders.append(current_row)
-    return orders     
+        drones.append(current_row)
+    return drones
+        
+
+def main():
+    res = csv_orders_to_system("../delivery_center1.csv")    
+    print(res)
+    
+if __name__ == "__main__":
+    main()    
          
