1414152435c0286e48892215aa559281ed8dbd5d
tomaspm98 <up202006845@up.pt>
2024-03-26 17:34:38 +0000

    Heuristics + greedy combinations (not sure)

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import re
+import math
 
 def msg_orders_to_list(msg):
     orders = msg.body.split(" ")
@@ -39,12 +40,27 @@ def csv_drones_to_system(csv):
         current_row = df.iloc[i].values.tolist()
         drones.append(current_row)
     return drones
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    dLat = (lat2 - lat1) * math.pi / 180.0
+    dLon = (lon2 - lon1) * math.pi / 180.0
+    
+    lat1 = (lat1) * math.pi / 180.0
+    lat2 = (lat2) * math.pi / 180.0
+    
+    a = (pow(math.sin(dLat / 2), 2) +
+         pow(math.sin(dLon / 2), 2) *
+             math.cos(lat1) * math.cos(lat2));
+    rad = 6371
+    c = 2 * math.asin(math.sqrt(a))
+    return rad * c
         
 
-def main():
-    res = csv_centers_to_system("../centers/delivery_center1.csv")    
-    print(res)
-    
 if __name__ == "__main__":
-    main()    
+    lat1 = 19.01
+    lon1 = 72.92
+    lat2 = 18.994
+    lon2 = 72.8255
+     
+    print(haversine_distance(lat1, lon1,lat2, lon2), "K.M.")  
          
