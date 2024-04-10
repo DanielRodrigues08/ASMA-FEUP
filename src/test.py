7f52b30f825321a252c2e0be8102de01648e642e
Daniel Rodrigues <up202006562@up.pt>
2024-04-10 12:38:50 +0100

    refactoring drone

--- src/test.py
+++ src/test.py
@@ -1,7 +1,64 @@
-import json
+import spade
+from spade.agent import Agent
+from spade.behaviour import OneShotBehaviour, CyclicBehaviour
+from spade.message import Message
+from spade.template import Template
 
 
-a = json.dumps({"type": "ORDERS_READY", "orders": 2})
+class SenderAgent(Agent):
+    class InformBehav(OneShotBehaviour):
+        async def run(self):
+            print("InformBehav running")
+            msg = Message(to="drone1@localhost")     # Instantiate the message
+            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+            msg.body = "Hello World"                    # Set the message content
 
-b = json.loads(a)
-print(b)
+            await self.send(msg)
+            print("Message sent!")
+
+            # stop agent from behaviour
+            await self.agent.stop()
+
+    async def setup(self):
+        print("SenderAgent started")
+        b = self.InformBehav()
+        self.add_behaviour(b)
+
+class ReceiverAgent(Agent):
+    class RecvBehav(CyclicBehaviour):
+        async def run(self):
+            #print("RecvBehav running")
+
+            msg = await self.receive(timeout=0) # wait for a message for 10 seconds
+            if msg:
+                print("Message received with content: {}".format(msg.body))
+            else:
+                #print("Did not received any message after 10 seconds")
+                pass
+            # stop agent from behaviour
+
+    async def setup(self):
+        print("ReceiverAgent started")
+        b = self.RecvBehav()
+        template = Template()
+        template.set_metadata("performative", "inform")
+        template.sender = "drone2@localhost"
+        self.add_behaviour(b, template)
+
+
+
+async def main():
+    receiveragent = ReceiverAgent("drone1@localhost", "drone1")
+    await receiveragent.start(auto_register=True)
+    print("Receiver started")
+
+    senderagent = SenderAgent("drone2@localhost", "drone2")
+    await senderagent.start(auto_register=True)
+    print("Sender started")
+
+    await spade.wait_until_finished(receiveragent)
+    print("Agents finished")
+
+
+if __name__ == "__main__":
+    spade.run(main())
